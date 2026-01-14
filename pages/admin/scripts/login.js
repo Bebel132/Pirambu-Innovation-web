bbfbc258b61eeefaeaddad94fc0854e09156e1e6
emanuel.ferreira <emanuel2005batista@gmail.com>
2026-01-14 14:52:02 -0300

    corrige erros

--- pages/admin/scripts/login.js
+++ pages/admin/scripts/login.js
@@ -2,12 +2,10 @@ import { API_URL } from "./../../../assets/config.js";
 
 async function initLogin() {
         const loading = document.querySelector(".loading")
-        const login = document.querySelector(".login")
         const unauthorized = document.querySelector("#unauthorized")
         
         const googleBtn = document.querySelector("#google")
         const microsoftBtn = document.querySelector("#microsoft")
-        //const logoutBtn = document.querySelector("#logout-btn")
 
         loading.style.display = "none"
         unauthorized.style.display = "none"
@@ -20,19 +18,12 @@ async function initLogin() {
             window.location.href = `${API_URL}/auth/microsoft/login`
         }
 
-        // logoutBtn.onclick = async () => {
-        //     loading.style.display = "flex"
-        //     await fetch(`${API_URL}/auth/logout`, {method: "POST", credentials: "include"})
-        //     loading.style.display = "none"
-        //     location.reload();
-        // }
-
         const teste = await fetch(`${API_URL}/auth/profile`, {
             credentials: "include"
         })
 
         if(teste.ok) {
-            window.location = "./courses/../courses/courses.html"
+            window.location = "courses/courses.html"
         }
 
         const url = new URL(window.location.href);
