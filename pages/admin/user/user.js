5bbb730896e0f5eaa99797f3ad36bf35a1d732cd
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2026-01-18 09:02:46 -0300

    coloca foto de perfil se o usuário não tiver

--- pages/admin/user/user.js
+++ pages/admin/user/user.js
@@ -5,7 +5,13 @@ async function initUser() {
 
     document.querySelector("#nome").textContent += user.username;
     document.querySelector("#email").textContent += user.email;
-    document.querySelector("#imagemDePerfil").src = user.picture;
+    console.log(user)
+    if(user.picture != null) {
+        document.querySelector("#imagemDePerfil").src = user.picture;
+    } else {
+        const userSplitedName = user.username.split(" ");
+        document.querySelector("#imagemDePerfil").src = `https://ui-avatars.com/api/?name=${userSplitedName[0]}+${userSplitedName[1]}`;
+    }
 
     document.querySelector("#logout").onclick = async () => {
         await api("auth/logout", { method: "POST", credentials: "include" });
