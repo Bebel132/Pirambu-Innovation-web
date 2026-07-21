21a7a0b30a0d4142939c4b35a477ddf52c8aaa9c
Emanuel Ferreira Batista <48385634+Bebel132@users.noreply.github.com>
2026-07-21 16:25:52 -0300

    Update API_URL to new production endpoint

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://pirambu-innovation-api.onrender.com'
+export const API_URL = 'https://arrange-preparing-contest-mpg.trycloudflare.com'
 // export const API_URL = 'http://127.0.0.1:5000'
 
 // caminho base do projeto
