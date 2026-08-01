dfa35fd96a844ef67a13895aa3c1b6dfb6c34e6c
EmanuelFerreira <48385634+Bebel132@users.noreply.github.com>
2026-08-01 11:18:14 -0300

    corrige rota da api

--- assets/config.js
+++ assets/config.js
@@ -1,15 +1,12 @@
 // export const API_URL = 'https://hitachi-refine-museums-convert.trycloudflare.com'
 const GIST_ID = 'c695194f270beb73384b82b9efc7ee90';
-const GIST_URL = `https://gist.githubusercontent.com/Bebel132/${GIST_ID}/raw/api-config.json`;
+const GIST_URL = `https://gist.githubusercontent.com/Bebel132/${GIST_ID}/raw/pirambu-api.json`;
 
 export let API_URL = '';
 
 export async function initApiUrl() {
     try {
-        // Cache-bust para sempre pegar a versão mais recente
-        const response = await fetch(`${GIST_URL}?t=${Date.now()}`);
-        const data = await response.json();
-        API_URL = data.api_url;
+        API_URL = await fetch(`${GIST_URL}?t=${Date.now()}`).then(res => res.json()).then(data => data.api_url+"");
     } catch (error) {
         console.error('Erro ao carregar API URL:', error);
         // Fallback para desenvolvimento
